56e7e95970dcf4503db7310984251f7072c4430f
joeyzhou <zhouyang0715@yahoo.com>
2023-06-09 00:47:14 -0400

    fixed something

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import * as petService from './services/petService'
 //pages
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
-import Landing from './pages/Landing/landing'
+import Landing from './pages/Landing/Landing'
 import PetList from './pages/PetList/petList'
 import PetDetails from './pages/PetDetails/PetDetails'
 import AddPet from './pages/AddPet/AddPet';
